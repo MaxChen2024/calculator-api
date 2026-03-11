ad67cbf3449b0e8383ee358ffba38b46ed6aaeb0
Max Chen <chen3805@purdue.edu>
2026-03-11 15:10:06 +0000

    bigquery initial connect

--- main.py
+++ main.py
@@ -1,7 +1,26 @@
-from fastapi import FastAPI, status, HTTPException
-import math
+# from fastapi import FastAPI, status, HTTPException
+# import math
+
+from fastapi import FastAPI, status, HTTPException, Depends
+from google.cloud import bigquery
+
 
 app = FastAPI()
+
+############################################################################################
+# Dependency method to provide a BigQuery client
+# This will be used by the other endpoints where a database connection is necessary
+def get_bq_client():
+    # client automatically uses Cloud Run's service account credentials
+    client = bigquery.Client()
+    try:
+        yield client
+    finally:
+        client.close()
+
+############################################################################################
+
+
 
 ## Health Check
 @app.get("/", status_code=200)
@@ -39,7 +58,7 @@ def add(a: str, b: str):
     return {"result": a + b}
 
 
-
+## Subtraction
 @app.get("/subtract/{a}/{b}", status_code=200)
 def subtract(a: str, b: str):
     """
@@ -62,6 +81,7 @@ def subtract(a: str, b: str):
     return {"result": a - b}
 
 
+## Multiplication
 @app.get("/multiply/{a}/{b}", status_code=200)
 def multiply(a: str, b: str):
     """
@@ -112,7 +132,7 @@ def divide(a: str, b: str):
 
 
 ####################
-## Additional Endpoints
+## Custom Endpoints
 ####################
 
 @app.get("/power/{a}/{b}", status_code=200)
@@ -183,3 +203,45 @@ def add_3(a: str, b: str, c: str):
         raise HTTPException(status_code=status.HTTP_422_UNPROCESSABLE_ENTITY, detail="'a', 'b', and 'c' must all be valid numbers")
 
     return {"result": a + b + c}
+
+    #####################################################################################
+    #####################################################################################
+
+@app.get("/dbwritetest", status_code=200)
+def dbwritetest(bq: bigquery.Client = Depends(get_bq_client)):
+    """
+    Writes a simple test row to a BigQuery table.
+
+    Uses the `get_bq_client` dependency method to establish a connection to BigQuery.
+    """
+    # Define a Python list of objects that will become rows in the database table
+    # In this instance, there is only a single object in the list
+    row_to_insert = [
+        {
+            "endpoint": "/dbwritetest",
+            "result": "Success",
+            "status_code": 200
+        }
+    ]
+    
+    # Use the BigQuery interface to write our data to the table
+    # If there are errors, store them in a list called `errors`
+    # YOU MUST UPDATE YOUR PROJECT AND DATASET NAME BELOW BEFORE THIS WILL WORK!!!
+    errors = bq.insert_rows_json("YOUR-PROJECT.calculator.api_logs", row_to_insert)
+
+    # If there were any errors, raise an HTTPException to inform the user
+    if errors:
+        # Log the full error to your Cloud Run logs for debugging
+        print(f"BigQuery Insert Errors: {errors}")
+        
+        # Raise an exception to the API user
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            detail={
+                "message": "Failed to log data to BigQuery",
+                "errors": errors  # Optional: return specific BQ error details
+            }
+        )
+
+    # If there were NOT any errors, send a friendly response message to the API caller
+    return {"message": "Log entry created successfully"}
